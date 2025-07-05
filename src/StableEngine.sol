052bac8c41d4af9ec8cd9b8121008bb9c9c462d6
Jnyandeep <thokchom.jnyandeep@gmail.com>
2025-07-05 18:24:33 +0530

    feat: priceFeed functionality

--- src/StableEngine.sol
+++ src/StableEngine.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.20;
 
 import "./StableCoin.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract StableEngine {
+    address public wBTCPriceFeed;
+
     IERC20 public wBTC;
     StableCoin public stableCoin;
 
-    constructor(address _wBTC, address _stableCoin) {
+    constructor(address _wBTC, address _stableCoin, address _wBTCPriceFeed) {
         wBTC = IERC20(_wBTC);
         stableCoin = StableCoin(_stableCoin);
+        wBTCPriceFeed = _wBTCPriceFeed;
     }
 
     function depositCollateral() external {}
@@ -19,5 +23,12 @@ contract StableEngine {
 
     function liquidate() external {}
 
-    function _checkHealthFactor() external view returns (uint256 healthFactor) {}
+    function _checkHealthFactor() internal view returns (uint256 healthFactor) {}
+
+    function _getwBTCPrice() internal view returns (uint256 price) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(wBTCPriceFeed);
+        (, int256 priceInt,,,) = priceFeed.latestRoundData();
+        require(priceInt > 0, "Invalid price");
+        return uint256(priceInt);
+    }
 }
